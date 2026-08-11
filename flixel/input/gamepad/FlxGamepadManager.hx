0cb51998e0d5a2c925aa2718c1eb7a4ae030f81b
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2026-08-11 18:54:40 +0300

    hmm

--- flixel/input/gamepad/FlxGamepadManager.hx
+++ flixel/input/gamepad/FlxGamepadManager.hx
@@ -52,6 +52,12 @@ class FlxGamepadManager implements IFlxInputManager
 	public var deviceDisconnected(default, null):FlxTypedSignal<FlxGamepad->Void>;
 
 	/**
+	 * Whether the bottom or right face button is ACCEPT
+	 * @since 5.9.0
+	 */
+	public var acceptMode:FlxGamepadAcceptMode = BOTTOM;
+
+	/**
 	 * Stores all gamepads - can have null entries, but index matches event.device
 	 */
 	var _gamepads:Array<FlxGamepad> = [];
@@ -594,4 +600,29 @@ class FlxGamepadManager implements IFlxInputManager
 				count++;
 		return count;
 	}
+}
+
+
+/**
+ * @since 5.9.0
+ */
+enum FlxGamepadAcceptMode
+{
+	/**
+	 * The bottom face button is `ACCEPT` and the right face button is `CANCEL`.
+	 * This is common on western-style consoles, like XBox or American PS4/5
+	 */
+	BOTTOM;
+	
+	/**
+	 * The right face button is `ACCEPT` and the bottom face button is `CANCEL`.
+	 * This is common in Japanese PS4/5 consoles, and Nintendo consoles
+	 */
+	RIGHT;
+	
+	/**
+	 * Behaves like `BOTTOM` for nearly all gamepads, but `RIGHT` for specific mappings,
+	 * namely Nintendo Switch gamepads
+	 */
+	USE_MAPPING;
 }
